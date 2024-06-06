ad676dc272eb87be5921feddc293af681ab0083e
martync96 <martynclow@gmail.com>
2024-06-06 15:57:32 +0100

    SavedLocationsDropdown testing finished

--- frontend/src/components/SavedLocationsDropdown.jsx
+++ frontend/src/components/SavedLocationsDropdown.jsx
@@ -15,7 +15,7 @@ const SavedLocationsDropdown = (props) => {
     };
 
     const favouriteLocationsMap = props.favouriteLocations.map((data, index) => 
-        <a key={index} className="dropdown-item" onClick={() => navigate(`/weather/${data.city}`)}>
+        <a key={index} className="dropdown-item" onClick={() => navigate(`/weather/${data.city}`)} data-testid='fav-map-item'>
             {data.city}
         </a>
     );
@@ -26,10 +26,10 @@ const SavedLocationsDropdown = (props) => {
         <>
         <li className={`nav-item dropdown ${props.favouriteLocations.length != 0 ? '' : 'favourites-dropdown-hidden'}`} data-testid='fav-dropdown'>
                                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" 
-                                aria-haspopup="true" aria-expanded="false" onClick={handleDropdown}>
+                                aria-haspopup="true" aria-expanded="false" onClick={handleDropdown} data-testid="fav-dropdown-tog">
                                     My Saved Locations
                                 </a>
-                                <div className={`dropdown-menu ${dropdown ? 'show' : ''}`} aria-labelledby="navbarDropdownMenuLink">
+                                <div className={`dropdown-menu ${dropdown ? 'show' : ''}`} aria-labelledby="navbarDropdownMenuLink" data-testid='dropdown-menu'>
                                     {favouriteLocationsMap}
                                     <a className="faveLocations" onClick={navigateToFavouriteLocations}>View All Favourite Locations</a>
                                 </div>
--- frontend/tests/SavedLocationsDropdown.test.jsx
+++ frontend/tests/SavedLocationsDropdown.test.jsx
@@ -1,7 +1,17 @@
 import { MemoryRouter } from "react-router-dom";
 import NavBar from "../src/components/NavBar";
-import { expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { expect, vi } from "vitest";
+import { render, screen, userEvent, fireEvent } from "@testing-library/react";
+
+const mockNavigate = vi.fn();
+
+vi.mock('react-router-dom', async (importOriginal) => {
+    const actual = await importOriginal()
+    return {
+      ...actual,
+      useNavigate: () => mockNavigate,
+    };
+  });
 
 describe(`SavedLocationsDropdown tests`, () => {
     it(`should render the dropdown if there are favourite locations`, () => {
@@ -30,5 +40,60 @@ describe(`SavedLocationsDropdown tests`, () => {
         } else {
             expect(dropdown).toBeNull();
         }
+    });
+
+    it(`should render the correct number of favourite locations in the dropdown`, () => {
+        render(
+            <MemoryRouter>
+                <NavBar favouriteLocations={[{city: 'London'}, {city: 'Paris'}, {city: 'New York'}]}/>
+            </MemoryRouter>
+        )
+
+        const dropdownItems = screen.queryAllByTestId('fav-map-item');
+
+        expect(dropdownItems).toHaveLength(3);
+    });
+
+    it(`should navigate to the correct page when a favourite location is clicked`, async () => {
+        render(
+            <MemoryRouter>
+                <NavBar favouriteLocations={[{city: 'London'}]}/>
+            </MemoryRouter>
+        )
+
+        const dropdownItem = screen.getByText('London');
+
+        await fireEvent.click(dropdownItem);
+
+        expect(mockNavigate).toHaveBeenCalledWith('/weather/London');
+
+    });
+
+    it(`should render the dropdown items when the dropdown is clicked`, async () => {
+        render(
+            <MemoryRouter>
+                <NavBar favouriteLocations={[{city: 'London'}]}/>
+            </MemoryRouter>
+        )
+
+        const dropdownToggle = screen.getByTestId('fav-dropdown-tog');
+        fireEvent.click(dropdownToggle);
+      
+        const dropdownMenu = screen.getByTestId('dropdown-menu');
+        expect(dropdownMenu).toHaveClass('show');
+
+    });
+
+    it(`should navigate to the favourite-locations page when the 'View All Favourite Locations' link is clicked`, async () => {
+        render(
+            <MemoryRouter>
+                <NavBar favouriteLocations={[{city: 'London'}]}/>
+            </MemoryRouter>
+        )
+
+        const viewAllLink = screen.getByText('View All Favourite Locations');
+
+        await fireEvent.click(viewAllLink);
+        expect(mockNavigate).toHaveBeenCalledWith('/favourite-locations');
     });
 });
